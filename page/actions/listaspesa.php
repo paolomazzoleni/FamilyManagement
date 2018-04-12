754d2071bc6d0c3cc02cbdb132f687d15622e7ba
Paolo Mazzoleni <33895675+paolomazzoleni@users.noreply.github.com>
2018-04-12 10:40:46 +0200

    Added graphics
    Modified the view of the shopping list section

--- page/actions/listaspesa.php
+++ page/actions/listaspesa.php
@@ -39,11 +39,11 @@
       x = document.getElementById("ins_date").value;
       var date_d = new Date(x);
       var today = new Date();
-      if (x == "") {
+      if (x == ""){
         alert("Errore: non hai compilato il campo data di scadenza");return;
       }
       else if(date_d<today){
-        alert("Errore: hai inserito una data di scadenza invalida");return;
+        alert("Errore: hai inserito una data");return;
       }
       //Se è tutto giusto
       document.getElementById("ins").submit();
@@ -60,7 +60,6 @@
       //se è richiesto INSERIMENTO
       if(isset($_REQUEST['ins_date'])){
         $sql = "INSERT INTO listaspesa (data,luogo,codice_fam) VALUES ('".$_REQUEST['ins_date']."','".$_REQUEST['ins_luo']."','".$_SESSION['fam']."')";
-
         if ($conn->query($sql) === FALSE) {
           echo "Error: " . $sql . "<br>" . $conn->error;
         }
@@ -76,25 +75,63 @@
       $sql = "SELECT * FROM listaspesa WHERE codice_fam='".$_SESSION['fam']."'";
 	  $result = $conn->query($sql);
       if ($result->num_rows > 0){
-          echo "<table><tr><th>ID</th><th>DATA</th><th>LUOGO</th></tr>";
+	      $i=0;
+          echo "
+		  <h4>Sono presenti delle spese</h4>
+		  <div id='accordion'>
+		  ";
           while($row = $result->fetch_assoc()){
-            echo "<tr><td>".$row['id_spesa']."</td><td>".$row['data']."</td><td>".$row['luogo']."</td></tr>";
+		    echo "
+			<div class='card'>
+				<div class='card-header' id='heading".$i."'>
+					<h5 class='mb-0'>
+					  <button class='btn btn-link' data-toggle='collapse' data-target='#".$i."' aria-expanded='true' aria-controls='collapseOne'>
+						Lista della spesa #".$row['id_spesa']."
+					  </button>
+					</h5>
+				</div>
+				<div id='".$i."' class='collapse' aria-labelledby='headingOne' data-parent='#accordion'>
+					<div class='card card-body'>
+						Informazioni
+						<ul>
+						  <li>Data: ".$row['data']."</li>
+						  <li>Luogo: ".$row['luogo']."</li>
+						</ul>";
+						$sql = "SELECT * FROM prodotto WHERE id_spesa='".$row['id_spesa']."'";
+						$result1 = $conn->query($sql);
+						if ($result1->num_rows > 0) {
+							echo "Prodotti<ul>";
+							while($row1 = $result1->fetch_assoc()) {
+								echo "<li>#".$row1['id_prod']." -- ".$row1['descrizione']."</li>";
+							}
+							echo "</ul>";
+						} 
+						else {
+							echo "<br>Questa lista della spesa non ha ancora prodotti";
+						}
+			echo 
+			" </div></div>
+			</div>";
+			$i++;
           }
-          echo "</table>";
+		  echo "</div>";
       } 
       else{
-          echo "ancora nessuna lista della spesa inserita.";
+          echo "
+            <div class='container-fluid mt-3'>
+              <div class='row'>
+                <div class='col'></div>
+                <div class='col'>
+                  <h2 align='center'>Nessuna lista della spesa ancora settata</h2>
+                  <p align='center'>Utilizza il menù sottostante per inserire la prima</p>
+                </div>
+                <div class='col'></div>
+              </div>
+            </div>
+          ";
       }
       echo "<br><br>----------------------------------------<br><br>";
-  
-      //menu visualizzazione
-      echo "<b>MENU VISUALIZZAZIONE</b>
-            <form action='visualizza.php' method='post'>
-            <input type='text' name='vis_id' placeholder='ID'><br>
-            <input type='submit' value='visualizza' name='vis'>
-            </form>";
-      echo "<br><br>----------------------------------------<br><br>";
-      
+
       //inserimento
       echo "<b>MENU INSERIMENTO</b>
             <form method='post' id='ins'>
@@ -103,7 +140,7 @@
             <input type='button' value='inserisci' onclick='controlla()' name='ins'>
             </form>";
       echo "<br><br>----------------------------------------<br><br>";
-  
+
       //menu cancellazione
       echo "<b>MENU CANCELLAZIONE</b>
             <form method='post'>
