e5f3a742d6452f5046f87f96ef76fe2d28fbb329
Paolo Mazzoleni <33895675+paolomazzoleni@users.noreply.github.com>
2018-04-15 14:39:29 +0200

    Fixed menu_fam.php indentation

--- page/menu_fam.php
+++ page/menu_fam.php
@@ -11,27 +11,26 @@
     $_SESSION['curpage']='home';
 ?>
 <html>
-  <head>
-    <title>Home | FM</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
-  <body style='background-color:#9ECCFF;'>
+	<head>
+		<title>Home | FM</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body style='background-color:#9ECCFF;'>
     <?php
-      require './_navbar.php';
-	  
-      $sql = "SELECT * FROM utente WHERE email='".$_SESSION['user']."'";
-      $result = $conn->query($sql);
-      $row = $result->fetch_assoc();
-
-      echo 
-        "<div class='mt-3' align='center' style='background-color:#DDDDDD;'>
-          <p style='font-size: large;'> <b>Benvenuto ".$row['nome']."</b><br>
-          eccoti alcune delle principali notizie odierne</p>
-        </div>";
+		require './_navbar.php';
+		$sql = "SELECT * FROM utente WHERE email='".$_SESSION['user']."'";
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+		echo "
+			<div class='mt-3' align='center' style='background-color:#DDDDDD;'>
+				<p style='font-size: large;'> <b>Benvenuto ".$row['nome']."</b><br>
+				eccoti alcune delle principali notizie odierne</p>
+			</div>
+		";
         
         //$feed_url='http://xml2.corriereobjects.it/rss/homepage.xml';
         $feed_url='http://www.liberoquotidiano.it/rss.jsp?sezione=1'; 
@@ -46,30 +45,35 @@
             $img = $array[1];
 			preg_match("/(http|https).*\.(jpg|JPG)/",$img,$matches,PREG_OFFSET_CAPTURE);
             //se non c'è l'immagine dell'articolo
-            if(empty($matches[0][0]))
-              $matches[0][0] = "http://familymanagement.altervista.org/img/news.jpg";
+            if(empty($matches[0][0])){
+				$matches[0][0] = "http://familymanagement.altervista.org/img/news.jpg";
+			}
 			  
-              if($i==0||$i==4){
-                echo "<div class='row'>";
-              }
+            if($i==0||$i==4){
+				echo "<div class='row'>";
+            }
               
-              echo "<div align='center' class='col mt-3'><div class='card' style='width: 18rem;height:400px;'>
-                <img class='card-img-top' src='".$matches[0][0]."'>
-                <div class='card-body'>
-                  <p class='card-text'>".$item->title."</p>
-                  <a target='_blank' href='".$item->link."' class='btn btn-primary'>Leggi l'articolo</a>
-                </div>
-              </div></div>";
+            echo "
+					<div align='center' class='col mt-3'><div class='card' style='width: 18rem;height:400px;'>
+						<img class='card-img-top' src='".$matches[0][0]."'>
+						<div class='card-body'>
+							<p class='card-text'>".$item->title."</p>
+							<a target='_blank' href='".$item->link."' class='btn btn-primary'>Leggi l'articolo</a>
+						</div>
+					</div>
+				</div>
+			";
               
-              if($i==3||$i==7){
-                echo "</div>";
-              }
+            if($i==3||$i==7){
+				echo "</div>";
+            }
             
             $i++;
-            if($i==8)
-              break;
+            if($i==8){
+				break;
+			}
         }
         echo "</div>";
 	?> 
-  </body>
+	</body>
 </html>
