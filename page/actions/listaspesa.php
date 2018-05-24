5463bea9f3b6b0d0a08684a6577a2377cac6e653
Paolo Mazzoleni <33895675+paolomazzoleni@users.noreply.github.com>
2018-05-24 12:44:14 +0200

    Added icon reference in pages

--- page/actions/listaspesa.php
+++ page/actions/listaspesa.php
@@ -5,7 +5,13 @@
 <html>
     <head>
 		<title>Lista spese | Famiglia</title>
-		<style>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <link rel="icon" href="http://familymanagement.altervista.org/img/favicon.ico"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
 			table {
 				border-collapse: collapse;
 			}
@@ -21,11 +27,6 @@
               	background-size: cover;
           	}
 		</style>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<script>
 		function controlla(){
