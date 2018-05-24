616557d4f98a20251c94b1ce0373135911f8a7f9
Paolo Mazzoleni <33895675+paolomazzoleni@users.noreply.github.com>
2018-05-24 12:42:12 +0200

    Added icon reference in pages

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 	<head>
 		<title>Family management</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-		<!--<link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <link rel="icon" href="http://familymanagement.altervista.org/img/favicon.ico"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<style>
           body{
