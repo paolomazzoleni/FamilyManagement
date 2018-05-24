37211d04bd8f75236a9c9112fd104dcc2100c307
Paolo Mazzoleni <33895675+paolomazzoleni@users.noreply.github.com>
2018-05-24 09:58:09 +0200

    Fixed navbar align
    - align = 'center'

--- page/_navbar.php
+++ page/_navbar.php
@@ -1,7 +1,7 @@
 <?php
 	echo "
     <nav class='navbar navbar-expand-lg navbar-dark bg-primary sticky-top'>
-		<span class='navbar-brand mb-0 h1'>Family Management</span>
+		<span align='center' class='navbar-brand mb-0 h1'>Family Management</span>
 		<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
 			<span class='navbar-toggler-icon'></span>
 		</button>
@@ -13,14 +13,14 @@
 	if($_SESSION['curpage']=='home'){
 		echo "
 			<li class='nav-item active'>
-				<a class='nav-link' href='http://familymanagement.altervista.org/page/menu_fam.php'>Home <span class='sr-only'>(current)</span></a>
+				<a class='nav-link' align='center' href='http://familymanagement.altervista.org/page/menu_fam.php'>Home <span class='sr-only'>(current)</span></a>
 			</li>
 		";
 	}
 	else{
 		echo "
 			<li class='nav-item'>
-				<a class='nav-link' href='http://familymanagement.altervista.org/page/menu_fam.php'>Home <span class='sr-only'>(current)</span></a>
+				<a class='nav-link' align='center' href='http://familymanagement.altervista.org/page/menu_fam.php'>Home <span class='sr-only'>(current)</span></a>
 			</li>
 		";
 	}
@@ -28,14 +28,14 @@
 	if($_SESSION['curpage']=='calendario'){
 		echo "
 			<li class='nav-item active'>
-				<a class='nav-link' href='http://familymanagement.altervista.org/page/actions/calendar.php'>Calendario</a>
+				<a class='nav-link' align='center' href='http://familymanagement.altervista.org/page/actions/calendar.php'>Calendario</a>
 			</li>
 		";
 	}
 	else{
 		echo "
 			<li class='nav-item'>
-				<a class='nav-link' href='http://familymanagement.altervista.org/page/actions/calendar.php'>Calendario</a>
+				<a class='nav-link' align='center' href='http://familymanagement.altervista.org/page/actions/calendar.php'>Calendario</a>
 			</li>
 		";
 	}
@@ -43,14 +43,14 @@
 	if($_SESSION['curpage']=='spsgen'){
 		echo "
 			<li class='nav-item active'>
-				<a class='nav-link' href='http://familymanagement.altervista.org/page/actions/spesgeneral.php'>Spese generali</a>
+				<a class='nav-link' align='center' href='http://familymanagement.altervista.org/page/actions/spesgeneral.php'>Spese generali</a>
 			</li>
 		";
 	}
 	else{
 		echo "
 			<li class='nav-item'>
-				<a class='nav-link' href='http://familymanagement.altervista.org/page/actions/spesgeneral.php'>Spese generali</a>
+				<a class='nav-link' align='center' href='http://familymanagement.altervista.org/page/actions/spesgeneral.php'>Spese generali</a>
 			</li>
 		";
 	}
@@ -58,20 +58,20 @@
 	if($_SESSION['curpage']=='lstsps'){
 		echo "
 			<li class='nav-item active'>
-				<a class='nav-link' href='http://familymanagement.altervista.org/page/actions/listaspesa.php'>Liste spesa</a>
+				<a class='nav-link' align='center' href='http://familymanagement.altervista.org/page/actions/listaspesa.php'>Liste spesa</a>
 			</li></ul>
 		";
 	}
 	else{
 		echo "
 			<li class='nav-item'>
-				<a class='nav-link' href='http://familymanagement.altervista.org/page/actions/listaspesa.php'>Liste spesa</a>
+				<a class='nav-link' align='center' href='http://familymanagement.altervista.org/page/actions/listaspesa.php'>Liste spesa</a>
 			</li></ul>
 		";
 	}
 	//bottoni fine navbar
 	echo "
-			<form class='form-inline my-lg-0' action='http://familymanagement.altervista.org/page/actions/f_settings.php'>
+			<form class='form-inline mt-1 my-lg-0' action='http://familymanagement.altervista.org/page/actions/f_settings.php'>
 				<button class='btn btn-secondary btn-md btn-block my-sm-0 mt-2 mr-2 ml-2' type='submit'>Impostazioni familiari</button>
 			</form>
 			<form class='form-inline my-lg-0' action='http://familymanagement.altervista.org/page/actions/p_settings.php'>
